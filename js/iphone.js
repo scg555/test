c5c0c297ae08cf2bb01204238d6f2b04ea95a22e
scg555 <402352600@qq.com>
2017-05-24 23:38:01 +0800

    测试项目

---
+++ js/iphone.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/5/17.
+ */
+$('[data-toggle="mask"]').hover(function () {
+  $(this).animate({top:'0'},150);
+},function () {
+  $(this).animate({top:'66%'},150);
+});
